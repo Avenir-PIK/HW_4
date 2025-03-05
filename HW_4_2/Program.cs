3e0da0fd1bdf7fbffd873b9d2425719a207e4835
Avenir Baryshev <avenirpik@gmail.com>
2025-03-05 11:54:13 +0700

    3 - ready

--- HW_4_2/Program.cs
+++ HW_4_2/Program.cs
@@ -1,24 +1,24 @@
-﻿namespace HW_4_1
+﻿namespace HW_4_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Вводится натуральное число n. Найти n! Например, 6! = 1 * 2 * 3 * 4 * 5 * 6.");
+            Console.WriteLine("Вводится натуральное число n. Найти 1 + 1/2 + 1/3 + … + 1/n");
             int dig = 0;
-            int fact = 1;
+            double drob = 1;
             // не даём ввести отрицательное не положительное (на целое не проверяем: лень)
             while (dig <= 0)
             {
                 Console.WriteLine("\nВведите натуральное число!");
                 dig = Convert.ToInt32(Console.ReadLine());
             }
-            //находим факториал перемножением всего от 1 до dig
-            for (int i = 1; i <= dig; i++)
+            //находим что требуется
+            for (int i = 2; i <= dig; i++)
             {
-                fact *= i;
+                drob += 1.0/i;
             }
-            Console.WriteLine("\nФакториал {0} равен {1}", dig, fact);
+            Console.WriteLine("\nСтранное действие с {0} равно {1}", dig, drob);
             Console.WriteLine("\nНажмите любую клавишу для выхода");
             Console.ReadKey();
         }
---
+++ HW_4_3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HW_4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводятся целые числа a>0, b>0. Найти a^b. Не использовать класс Math");
+            int dig_a = 0;
+            int dig_b = 0;
+            // не даём ввести не по условию
+            while (dig_a <= 0)
+            {
+                Console.WriteLine("\nВведите целое число a > 0");
+                dig_a = Convert.ToInt32(Console.ReadLine());
+            }
+            while (dig_b <= 0)
+            {
+                Console.WriteLine("\nВведите целое число b > 0");
+                dig_b = Convert.ToInt32(Console.ReadLine());
+            }
+            //находим что требуется
+            double result = dig_a;
+            for (int i = 1; i < dig_b; i++)
+            {
+                result = result * dig_a;
+            }
+            Console.WriteLine("\n{0} в степени {1} равно {2}", dig_a, dig_b, result);
+            Console.WriteLine("\nНажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }
+    }
+}
