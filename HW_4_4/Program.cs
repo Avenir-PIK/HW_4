ba2a49905f7a80d59cad7b7dc12df0888fc3e725
Avenir Baryshev <avenirpik@gmail.com>
2025-03-05 12:31:19 +0700

    5 - rdy

---
+++ HW_4_4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace HW_4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводятся целые числа a>0, b<0. Найти a^b. Не использовать класс Math");
+            int dig_a = 0;
+            int dig_b = 0;
+            // не даём ввести не по условию
+            while (dig_a <= 0)
+            {
+                Console.WriteLine("\nВведите целое число a > 0");
+                dig_a = Convert.ToInt32(Console.ReadLine());
+            }
+            while (dig_b >= 0)
+            {
+                Console.WriteLine("\nВведите целое число b < 0");
+                dig_b = Convert.ToInt32(Console.ReadLine());
+            }
+            //находим что требуется
+
+            dig_b *= -1;
+            double result = dig_a;
+
+            for (int i = 1; i < dig_b; i++)
+            {
+                result = result * dig_a;
+            }
+
+            double result2 = 1 / result;
+            Console.WriteLine("\n{0} в степени -{1} равно {2}", dig_a, dig_b, result2);
+            Console.WriteLine("\nНажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW_4_5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HW_4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводятся целые числа a>0, b<0. Найти a^b. Не использовать класс Math");
+            double dig_a = 0;
+            do
+            {
+                Console.WriteLine("\nВведите число из диапазона [20;60]");
+                dig_a = Convert.ToInt32(Console.ReadLine());
+            }
+            while (dig_a < 20 || dig_a > 60);
+
+            Console.WriteLine("Спасибо!");
+            Console.WriteLine("\nНажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }
+    }
+}
